3bede457afbf50f0a51badc31375e077bd98a7e9
BereniceTovar <ybere_tovar@hotmail.com>
2018-06-21 17:35:06 -0500

    Se agregan funcionalidades

--- js/app.js
+++ js/app.js
@@ -34,13 +34,53 @@ $("#phone").keyup(function(){
     }
 });
 
-//Función que lanza código generado aleatoriamente
+//Función que lanza código generado aleatoriamente**
 $('#btn-next').click(function(){
     $('#phone').val('');
     $('#sign-up').hide();
+
+
+    //Función que lanza números
+function randomRage(min, max) {
+    return Math.floor(Math.random() *(max - min + 1)) + min;
+}
+
+var randomNumbers = randomRage(100, 600);
+swal("Your code: ", "LAB-" + randomNumbers, "success");
+
+var randomNumbers2 = randomRage(200, 700);
+    //Función que vuelve a enviar el número de 3 digitos
+$('#resend-code').click(function(){
+    swal("Your code: ", "LAB-" + randomNumbers2, "success");
+})
+
+//Función que activa el boton si coinciden el número escrito con la clave
+
+$("#lab-code").keyup(function(){
+    var labCode = $('#lab-code').val();
+    //$('#lab-code').val('');
+    if(labCode == randomNumbers || labCode == randomNumbers2) {
+    $("#btn-next2").removeAttr("disabled");
+    }
 });
 
+});
 
+//Función que habilita el último form cuando todos los campos esten llenos
+var valueForm = $('.val-form').val();
+if(valueForm != '' ){
+    console.log('Hola :B');
+}
+/*
+if( $('.form-check-input').prop('checked') ) {
+    alert('Seleccionado');
+}*/
+
+
+//Función click segundo form
+$('#btn-next2').click(function(){
+    $('#verify-phone').hide();
+});
 
 
 
